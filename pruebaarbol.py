1f63e5e7d1ef54568132cab1d64e1505ed1afe11
FerCafferata <fercafferata@outlook.com>
2021-04-09 14:51:37 -0300

    .

--- pruebaarbol.py
+++ pruebaarbol.py
@@ -1,56 +1,487 @@
-from tdaarbol import NodoArbol, busqueda, eliminar, inorden, altura
-from tdaarbol import postorden, preorden, insertar, reemplazar, act_altura
-from tdaarbol import balancear, rot_doble, rot_simple
+from tdaarbol import *
 import random
 r = None
-r = insertar(r, 4)
-r = insertar(r, 6)
-r = insertar(r, 3)
-r = insertar(r, 10)
-r = insertar(r, 1)
-r = insertar(r, 8)
-r = insertar(r, 15)
-r = insertar(r, 12)
-r = insertar(r, 5)
-preorden(r)
-# 4,6,3,10,1,8,15,12,5
 
-# ej1 TERMINAR
-'''r = None
-for i in range(0, 10):
-    r = insertar(r, random.randint(0, 100))'''
+# ej1
+for i in range(0, 1000):
+    r = insertar(r, random.randint(0, 2000))
 # Parte A
-'''print('Barrido preorden:')
+print('Barrido preorden:')
 preorden(r)
-print('Barrido inorden:')
+'''print('Barrido inorden:')
 inorden(r)
 print('Barrido postorden:')
 postorden(r)'''
+
 # Parte B
 '''n = 40
 if busqueda(r, n) is None:
     print('El numero no se encuentra en el arbol')
 else:
-    print('EL numero se encuentra en el arbol')'''
-# Parte C TERMINAR
+    print('EL numero se encuentra en el arbol')
+'''
+# Parte C
 '''for i in range(0, 3):
-    dato = int(input('Ingrese el numero que desea eliminar:'))
-    eliminar(r, dato)
-    preorden(r)'''
+    dato1 = 10
+    dato2 = 2
+    dato3 = 7
+    eliminar(r, dato1)
+    eliminar(r, dato2)
+    eliminar(r, dato3)
+print('Arbol con valores borrados')
+preorden(r)
+'''
 # Parte D
 '''aux = altura(r.izq)
 print('Altura del arbol izquierdo ' + str(aux))
 aux1 = altura(r.der)
-print('Altura del arbol derecho ' + str(aux1))'''
-# Parte E
+print('Altura del arbol derecho ' + str(aux1))
+'''
 
-hhkgkj
+# Parte E
+'''repetidos(r)
+'''
+
+
+# Parte F
+'''cp, ci = numParImpar(r)
+print('Cantidad de numeros pares: ', cp)
+print('Cantidad de numeros impares: ', ci)
+'''
+
+# Ej 2
+'''def expresion(r=None):
+    # expresion = (10/2) + (4*3)
+    r = NodoArbol('+')
+    r.izq = NodoArbol('/')
+    r.izq.izq = NodoArbol(10)
+    r.izq.der = NodoArbol(2)
+    r.der = NodoArbol('*')
+    r.der.izq = NodoArbol(4)
+    r.der.der = NodoArbol(3)
+    return r
+    # expresion = (15x2-10) - (8/4)
+    r = NodoArbol('-')
+    r.izq = NodoArbol('-')
+    r.izq.der = NodoArbol(10)
+    r.izq.izq = NodoArbol('*')
+    r.izq.izq.izq = NodoArbol(15)
+    r.izq.izq.der = NodoArbol(2)
+    r.der = NodoArbol('/')
+    r.der.izq = NodoArbol(8)
+    r.der.der = NodoArbol(4)
+    return r'''
+
+'''
+r = expresion()
+print('Preorden')
+preorden(r)
+print('La expresion se muestra en orden')
+inorden(r)
+print('Postorden')
+postorden(r)
+
+# punto B
+print('Resultado: ' + str(calcular(r)))
+'''
+
+# Ej 4
+'''r = NodoArbol(5)
+r.izq = NodoArbol(3)
+r.der = NodoArbol(7)
+r.izq.izq = NodoArbol(10)
+r.izq.der = NodoArbol(4)
+r.der.izq = NodoArbol(9)
+r.der.der = NodoArbol(2)
+print('Inorden')
+inorden(r)
+print('Hijo izquierdo')
+inorden(hijo_izquierdo(r))
+print('Hijo derecho')
+inorden(hijo_derecho(r))
+'''
+
 # ej5
-'''r = None
-"True heroe, false villano"
-super = ["Ironman ", "Spiderman ", "CapitanAmerica ", "Dr.Strange ",
-        "Hulk ", "BlackPanter "]
-for elemento in super:
-    r = insertar(r, random.choice(super) + str(random.choice([True, False])))
-preorden(r)'''
+# True heroe, false villano
+'''sh = ['Capitan America ', 'Thor ', 'Hulk ', 'Black Widow ', 'Iron Man ',
+        'Vision ', 'Hawkeye', 'Groot', 'Spider-man', 'Dr Strange']
+        
+villano = ['Thanos', 'Yellowjacket', 'Ultron', 'Red Skull', 'Iron Monger',
+            'Loki', 'Vulture', 'Whiplash', 'Crossbones', 'The Mandarin']
+contSH = 0
+for i in range(10):
+    r = insertar(r, [sh[i], True])
+    contSH += 1
+    r = insertar(r, [villano[i], False])'''
+'''preorden(r)
+print('Inorden')
+inorden(r)'''
+
+# Parte B
+'''print('Villanos ordenados alfabeticamente')
+villanos(r)'''
+
+# Parte C
+'''print('Superheroes con C')
+sh_con_c(r)'''
+
 # Parte D
+'''print('Cantidad de superheroes: ' + str(contSH))'''
+
+# Parte E
+'''buscar = busquedaProxCampo(r, 'Dr Strange', 0)
+if buscar is not None:
+    buscar.info[0] = 'Doctor Strange'
+    print('El nombre del personaje ha sido cambiado a ' + buscar.info[0])
+else:
+    print('El personaje no ha sido encontrado')
+
+print('Arbol con el nombre del personaje modificado')
+inorden(r)'''
+
+# Parte F
+'''print('Barrido inverso')
+barridoInvertido(r)'''
+
+# Parte G
+'''bosque = [None, None]
+separarHyV(r, bosque)
+'''
+# punto 1
+'''print('Cantidad de nodos del arbol superheroes: ' + str(pesoArbol(bosque[0])))
+print('Cantidad de nodos del arbol villanos: ' + str(pesoArbol(bosque[1])))'''
+
+# punto 2
+'''print('Arbol de superheroes')
+inorden(bosque[0])
+print('Arbol de villanos')
+inorden(bosque[1])'''
+
+
+# Ej7
+'''r = NodoArbol(5)
+r.izq = NodoArbol(3)
+r.der = NodoArbol(7)
+r.izq.izq = NodoArbol(10)
+r.izq.der = NodoArbol(4)
+r.der.izq = NodoArbol(9)
+r.der.der = NodoArbol(2)
+print('Inorden')
+inorden(r)
+print('Nodo minimo ', minimo(r).info)
+print('Nodo maximo ', maximo(r).info)
+'''
+
+
+# Ej 8
+'''tabla = [
+            [0.2, 'A'],
+            [0.17, 'F'],
+            [0.13, '1'],
+            [0.21, '3'],
+            [0.05, '0'],
+            [0.09, 'M'],
+            [0.15, 'T']
+        ]
+diccionario = {
+        'A': '00',
+        '3': '01',
+        '1': '100',
+        '0': '1010',
+        'M': '1011',
+        'T': '110',
+        'F': '111'
+    }
+
+tabla.sort()
+r = crearArbolHuffman(tabla)
+inorden(r)
+mensaje = 'FAMA'
+print('Mensaje a codificar: ', mensaje)
+mensaje_codificado = codificar(mensaje, diccionario)
+print('Mensaje comdificado: ', mensaje_codificado)
+print('Mensaje decodificado: ', decodificar(r, mensaje_codificado))
+'''
+
+# Ej 9
+'''r = NodoArbol(5)
+r.izq = NodoArbol(3)
+r.der = NodoArbol(7)
+r.izq.izq = NodoArbol(10)
+r.izq.der = NodoArbol(4)
+r.der.izq = NodoArbol(9)
+r.der.der = NodoArbol(2)
+inorden(r)
+imprimirArbol(r)
+nivel = 1
+print('En el nivel ', nivel, ' hay ', nodosxnivel(r, nivel), ' tendría que haber ', nodostotalXnivel(nivel), ' nodos')
+'''
+
+# Ej 10
+'''for i in range(0, 20):
+    r = insertar(r, i)
+inorden(r)
+'''
+# punto A
+'''
+print('Hay ' + str(pesoArbol(r)) + ' nodos en el arbol')
+'''
+
+# punto B y C
+'''print('Hay ' + str(contarHojas(r)) + ' hojas en el arbol')
+print('Informacion de las hojas del arbol')
+infoHojas(r)'''
+
+# Punto D
+'''nodo = 3
+if nodo == r.info:
+    print('El nodo ingresado es la raiz')
+else:
+    print('El padre de ' + str(nodo) + ' es ' + str(padre(r, nodo).info))
+'''
+# Punto E
+'''
+print('La altura del arbol es: ' + str(altura(r)))
+'''
+
+
+# Ej 11
+'''niveles = 9
+r = generarNiveles(niveles)
+imprimirArbol(r)
+
+# Punto A
+nivel = 3
+bosque = []
+cortarNiveles(r, bosque, nivel)
+print('Bosque sin los primeros tres niveles')
+for arbol in bosque:
+    print('------------------ Arbol ------------------')
+    imprimirArbol(arbol)
+'''
+# Punto B
+'''for arbol in bosque:
+    print('Hay ' + str(pesoArbol(arbol)) + ' nodos en el arbol')
+'''
+
+# punto C
+'''for arbol in bosque:
+    print('Barrido preorden')
+    preorden(arbol)
+    print()
+'''
+# Punto D
+'''aux = bosque[0]
+mayor = 0
+for arbol in bosque:
+    if mayor < pesoArbol(arbol):
+        mayor = pesoArbol(arbol)
+        aux = arbol
+print('El arbol con mayor cantidad de nodos tiene ', mayor, ' nodos y es el arbol con la raiz ', aux.info)
+'''
+
+# Punto E
+'''aux = bosque[0]
+for arbol in bosque:
+    if arbolcompleto(arbol):
+        print('El arbol con la raiz ', arbol.info, ' está completo')
+    else:
+        print('No hay ningun arbol completo')
+'''
+
+
+# Ej 12
+'''mision = 'Itergalactica'
+superheroe_para_mision(mision)'''
+
+
+# Ej 13
+'''tabla = [
+        ['E', 500],
+        ['T', 1500],
+        ['I', 250],
+        ['A', 750],
+        ['N', 1250],
+        ['M', 1750],
+        ['S', 225],
+        ['U', 275],
+        ['R', 725],
+        ['W', 775],
+        ['D', 1225],
+        ['K', 1275],
+        ['G', 1725],
+        ['O', 1775],
+        ['H', 112],
+        ['V', 237],
+        ['F', 265],
+        [' ', 285],
+        ['L', 612],
+        [' ', 737],
+        ['P', 765],
+        ['J', 785],
+        ['B', 1112],
+        ['X', 1237],
+        ['C', 1265],
+        ['Y', 1285],
+        ['Z', 1612],
+        ['Q', 1737],
+        [' ', 1765],
+        [' ', 1785],
+        ['5', 55],
+        ['4', 115],
+        ['3', 247],
+        ['2', 747],
+        ['1', 795],
+        ['6', 1055],
+        ['7', 1606],
+        ['8', 1755],
+        ['9', 1778],
+        ['0', 1788]
+]
+
+r = NodoArbolD(' ', 1000)
+for item in tabla:
+    r = insertarDecision(r, item[0], item[1])
+
+mensaje1 = '.--. .- ... . / .-.. --- / --.- ..- . / .--. .- ... . / -- .- .- -. .- / .--. .-. --- -- - .- -- . / .- .-.. --. --- / --.- ..- . / ... . --. ..- .. .-. / ... .. . -. -.. --- / ..- ... - . -.. / -. --- / ..- -. / ... --- .-.. -.. .- -.. --- / .--. . .-. ..-. . -.-. - --- / ... .. -. --- / ..- -. / -... ..- . -. / .... --- -- -... .-. . .-.-.'
+mensaje2 = '-. --- ... --- - .-. --- ... / ... --- -- --- ... / .-.. --- ... / -- .- .-.. -.. .. - --- ... / --. ..- .- .-. -.. .. .- -. . ... / -.. . / .-.. .- / --. .- .-.. .- -..- .. .- .-.-.'
+mensaje3 = '-.-- --- / ... --- .-.. --- / .- -.-. - ..- --- / -.-. --- -- --- / ... .. / . -. / ...- . .-. -.. .- -.. / .-.. --- / ... ..- .--. .. . .-. .- / - --- -.. --- .-.-.'
+mensaje4 = '-.-. .... .. -.-. --- ... / . ... - --- -.-- / .-.. .-.. . ...- .- -. -.. --- / .-.. .- / ..-. .. . ... - .- / .... .- -.-. .. .- / ..- ... - . -.. . ... .-.-.'
+mensaje5 = '.--. --- -.. .-. .. .- / .... .- -.-. . .-. / . ... - --- / - --- -.. --- / . .-.. / -.. .. .- .-.-.'
+m = '. ... / .-.. ..- -. . ...'
+print('Dr. Abraham Erskine')
+print(morse_decod(r, mensaje1))
+print('Rocket Raccoon')
+print(morse_decod(r, mensaje2))
+print('Natasha Romanoff')
+print(morse_decod(r, mensaje3))
+print('Tony Stark')
+print(morse_decod(r, mensaje4))
+print('Steve Rogers')
+print(morse_decod(r, mensaje5))
+'''
+
+
+# Ej 15
+'''tabla = [
+        [0.22, 'Despejado'],
+        [0.15, 'Nublado'],
+        [0.03, 'Lluvia'],
+        [0.26, 'Baja'],
+        [0.14, 'Alta'],
+        [0.05, '1'],
+        [0.01, '2'],
+        [0.035, '3'],
+        [0.06, '5'],
+        [0.02, '7'],
+        [0.025, '8']
+]
+diccionario = {
+            'Despejado': '00',
+            'Lluvia': '01011',
+            'Nublado': '111',
+            'Baja': '10',
+            'Alta': '110',
+            '1': '0100',
+            '2': '011100',
+            '3': '01111',
+            '5': '0110',
+            '7': '011101',
+            '8': '01010',
+}
+tabla.sort()
+r = crearArbolHuffman(tabla)
+
+mensaje = mensajeSatelite()
+print('Mensaje original: ', mensaje)
+mensaje_comprimido = codificar_mensajeSatelite(diccionario, mensaje)
+print('Mensaje codificado: ', mensaje_comprimido)
+mensaje_decodificado = decodificar(r, mensaje_comprimido)
+print('Mensaje decodificado: ', mensaje_decodificado)
+diferencia = diferencia_tamanio(mensaje_comprimido, mensaje_decodificado)
+print('Diferencia de tamaño: ', diferencia)
+'''
+
+# Ej 19
+'''clima = [
+        ['Visibilidad', 15],
+        ['Humedad', 70],
+        'Despejado',
+        ['Viento', 8.7],
+        ['Visibilidad', 8],
+        ['Viento', 5],
+        'Parcialmente Nublado',
+        ['Presion', 1013],
+        ['Humedad', 92],
+        'Despejado',
+        'Nublado',
+        ['Humedad', 96],
+        ['Viento', 7.2],
+        ['Visibilidad', 12],
+        ['Viento', 12.2],
+        'Nublado',
+        'Mayormente Nublado',
+        ['Presion', 1018],
+        'Nublado',
+        'Despejado',
+        'Mayormente Nublado',
+        'Lluvia',
+        'Nublado',
+        ['Visibilidad', 1],
+        'Nublado',
+        'Lluvia',
+        'Mayormente Nublado'
+]
+
+codigos = [3000, 1000, 3100, 500, 2100, 250, 750, 1400, 2500, 200, 300, 1300, 1900, 2300, 2700, 1100, 1200, 1700, 2000, 2200, 2400, 2600, 2800, 1500, 1800, 1450, 1600]
+for i in range(0, len(clima)):
+    r = insertarDecision(r, clima[i], codigos[i])
+
+temperatura = random.randint(0, 50)
+presion = random.randint(1000, 1050)
+humedad = random.randint(0, 100)
+visibilidad = random.randint(0, 50)
+viento = random.randint(0, 100)
+
+tiempo_meteorologico(r, temperatura, presion, humedad, visibilidad, viento)'''
+
+
+
+# ej 22
+'''tabla = [
+        [1, 'Q', 0],
+        [2, 'B', 0],
+        [2, 'V', 0],
+        [3, 'D', 0],
+        [3, 'G', 0],
+        [3, 'M', 0],
+        [3, 'T', 0],
+        [4, 'C', 0],
+        [4, 'P', 0],
+        [4, 'S', 0],
+        [4, 'U', 0],
+        [6, 'I', 0],
+        [6, 'L', 0],
+        [6, 'N', 0],
+        [7, 'O', 0],
+        [10, 'R', 0],
+        [11, 'A', 0],
+        [14, 'E', 0],
+        [17, '‘ ’', 0],
+        [2, ',', 0],
+]
+frecuencia(tabla)
+r = crearArbolHuffman(tabla)
+mensaje1 = '10001011101011000010111010001110000011011000000111100111101001011000011010011100110100010111010111111101000011110011111100111101000110001100000010110101111011111110111010110110111001110110111100111111100101001010010100000101101011000101100110100011100100101100001100100011010110101011111111111011011101110010000100101011000111111100010001110110011001011010001101111101011010001101110000000111001001010100011111100001100101101011100110011110100011000110000001011010111110011100'
+mensaje2 = '0110101011011100101000111101011100110111010110110100001000111010100101111010011111110111001010001111010111001101110101100001100010011010001110010010001100010110011001110010010000111101111010'
+mensaje1_decodificado = decodificar(r, mensaje1)
+mensaje2_decodificado = decodificar(r, mensaje2)
+print('Mensaje 1: ')
+print(mensaje1_decodificado)
+print('Mensaje 2: ')
+print(mensaje2_decodificado)
+tamanio1 = diferencia_tamanio(mensaje1, mensaje1_decodificado)
+print('Diferencia de tamanio entre mensaje1 codificado y decodificado: ', tamanio1)
+tamanio2 = diferencia_tamanio(mensaje2, mensaje2_decodificado)
+print('Diferencia de tamanio entre mensaje2 codificado y decodificado: ', tamanio2)
+'''
--- tdaarbol.py
+++ tdaarbol.py
@@ -1,9 +1,45 @@
+from tdalista import *
+import random
+import json
+from sys import getsizeof
+
+
 class NodoArbol():
     def __init__(self, info):
         self.izq = None
         self.der = None
         self.altura = 0
         self.info = info
+
+# Arbol de decision
+
+
+class NodoArbolD():
+    def __init__(self, info, num=0):
+        self.izq = None
+        self.der = None
+        self.num = num
+        self.info = info
+
+
+def insertarDecision(raiz, dato, num):
+    if raiz is None:
+        raiz = NodoArbolD(dato, num)
+    else:
+        if num < raiz.num:
+            raiz.izq = insertarDecision(raiz.izq, dato, num)
+        else:
+            raiz.der = insertarDecision(raiz.der, dato, num)
+    return raiz
+
+
+def imprimirArbol(raiz, espacios=0):
+    ''' Imprime arbol, girado hacia la izquierda'''
+    if raiz is not None:
+        espacios += 5
+        imprimirArbol(raiz.der, espacios)
+        print(" " * espacios, str(raiz.info))
+        imprimirArbol(raiz.izq, espacios)
 
 
 def altura(raiz):
@@ -72,7 +108,6 @@ def insertar(raiz, dato):
             raiz.der = insertar(raiz.der, dato)
     act_altura(raiz)
     raiz = balancear(raiz)
-    
     return raiz
 
 
@@ -87,6 +122,30 @@ def busqueda(raiz, buscado):
             else:
                 pos = busqueda(raiz.der, buscado)
     return pos
+
+
+def busquedaProximidad(raiz, buscado):
+    aux = None
+    if (raiz is not None):
+        if (buscado in raiz.info):
+            return raiz
+        else:
+            aux = busquedaProximidad(raiz.izq, buscado)
+            if aux is None:
+                aux = busquedaProximidad(raiz.der, buscado)
+    return aux
+
+
+def busquedaProxCampo(raiz, buscado, campo):
+    aux = None
+    if (raiz is not None):
+        if (buscado in raiz.info[campo]):
+            return raiz
+        else:
+            aux = busquedaProxCampo(raiz.izq, buscado, campo)
+            if aux is None:
+                aux = busquedaProxCampo(raiz.der, buscado, campo)
+    return aux
 
 
 def preorden(raiz):
@@ -121,20 +180,368 @@ def reemplazar(raiz):
 
 def eliminar(raiz, clave):
     x = None
-    if raiz is not None:
-        if raiz.info > clave:
+    if (raiz is not None):
+        if (raiz.info > clave):
             raiz.izq, x = eliminar(raiz.izq, clave)
-        elif raiz.info < clave:
-            raiz.der, x = eliminar(raiz.der, clave)
         else:
-            x = raiz.info
-            if raiz.izq is None:
-                x = raiz.info
-                raiz = raiz.der
-            elif raiz.der is None:
-                x == raiz.info
+            if(raiz.info < clave):
+                raiz.der, x = eliminar(raiz.der, clave)
+            else:
+                if (raiz.izq is None):
+                    x = raiz.info
+                    raiz = raiz.der
+                else:
+                    if(raiz.der is None):
+                        x = raiz.info
+                        raiz = raiz.izq
+                    else:
+                        raiz.izq, aux = reemplazar(raiz.izq)
+                        raiz.info = aux.info
+    return(raiz, x)
+
+
+def hoja(raiz):
+    if raiz.der is None and raiz.izq is None:
+        return True
+
+
+def repetidos(raiz):
+    if raiz is not None:
+        if busqueda(raiz.izq, raiz.info) is not None:
+            print('Repetido', raiz.info)
+        if busqueda(raiz.der, raiz.info) is not None:
+            print('Repetido', raiz.info)
+        repetidos(raiz.izq)
+        repetidos(raiz.der)
+
+
+def numParImpar(raiz):
+    if raiz is not None:
+        if raiz.info % 2 == 0:
+            return(1 + numParImpar(raiz.izq)[0] + numParImpar(raiz.der)[0], 0 + numParImpar(raiz.izq)[1] + numParImpar(raiz.der)[1])
+        else:
+            return(0 + numParImpar(raiz.izq)[0] + numParImpar(raiz.der)[0], 1 + numParImpar(raiz.izq)[1] + numParImpar(raiz.der)[1])
+    else:
+        return 0, 0
+
+
+def operacion(operador, der, izq):
+    resultado = 0
+    if operador == '*':
+        resultado = izq * der
+    elif operador == '/':
+        resultado = izq / der
+    elif operador == '+':
+        resultado = izq + der
+    elif operador == '-':
+        resultado = izq - der
+    return resultado
+
+
+def calcular(raiz):
+    if hoja(raiz):
+        return raiz.info
+    else:
+        return operacion(raiz.info, calcular(raiz.der), calcular(raiz.izq))
+
+
+def hijo_derecho(raiz):
+    return raiz.der
+
+
+def hijo_izquierdo(raiz):
+    return raiz.izq
+
+
+def villanos(raiz):
+    if raiz is not None:
+        villanos(raiz.izq)
+        if not raiz.info[1]:
+            print(raiz.info)
+        villanos(raiz.der)
+
+
+def sh_con_c(raiz):
+    if raiz is not None:
+        sh_con_c(raiz.izq)
+        if raiz.info[0][0] == 'C' and raiz.info[1]:
+            print(raiz.info)
+        sh_con_c(raiz.der)
+
+
+def barridoInvertido(raiz):
+    if raiz is not None:
+        barridoInvertido(raiz.der)
+        print(raiz.info)
+        barridoInvertido(raiz.izq)
+
+
+def separarHyV(raiz, bosque):
+    if raiz is not None:
+        separarHyV(raiz.izq, bosque)
+        separarHyV(raiz.der, bosque)
+        if raiz.info[1]:
+            bosque[0] = insertar(bosque[0], raiz.info)
+        else:
+            bosque[1] = insertar(bosque[1], raiz.info)
+
+
+def pesoArbol(raiz):
+    if raiz is not None:
+        return 1 + pesoArbol(raiz.izq) + pesoArbol(raiz.der)
+    else:
+        return 0
+
+
+def minimo(raiz):
+    aux = raiz
+    while aux.izq is not None:
+        aux = aux.izq
+    return aux
+
+
+def maximo(raiz):
+    aux = raiz
+    while aux.der is not None:
+        aux = aux.der
+    return aux
+
+
+def nodosxnivel(raiz, nivel, nivel_ingresado=0):
+    c = 0
+    if raiz is not None:
+        if nivel == nivel_ingresado:
+            c += 1
+        nivel_ingresado += 1
+        c += nodosxnivel(raiz.izq, nivel, nivel_ingresado)
+        c += nodosxnivel(raiz.der, nivel, nivel_ingresado)
+        return c
+    else:
+        return 0
+
+
+def nodostotalXnivel(nivel):
+    return 2**nivel
+
+
+def contarHojas(raiz):
+    if raiz is not None:
+        if raiz.izq is None and raiz.der is None:
+            return 1
+        else:
+            return contarHojas(raiz.izq) + contarHojas(raiz.der)
+    else:
+        return 0
+
+
+def infoHojas(raiz):
+    if raiz is not None:
+        infoHojas(raiz.izq)
+        if hoja(raiz):
+            print(raiz.info)
+        infoHojas(raiz.der)
+
+
+def padre(raiz, nodo):
+    aux = None
+    if raiz is not None:
+        if raiz.info == nodo:
+            aux = nodo
+        elif (raiz.izq is not None and raiz.izq.info == nodo) or (raiz.der is not None and raiz.der.info == nodo):
+            aux = raiz
+        else:
+            if nodo < raiz.info:
+                aux = padre(raiz.izq, nodo)
+            else:
+                aux = padre(raiz.der, nodo)
+    return aux
+
+
+class NodoHuffman():
+
+    def __init__(self, valor, dato, izq=None, der=None):
+        self.izq = izq
+        self.der = der
+        self.info = dato
+        self.valor = valor
+
+
+def crearArbolHuffman(lista):
+    nodos = []
+    for item in lista:
+        nodo = NodoHuffman(item[0], item[1])
+        nodos.append(nodo)
+    while len(nodos) > 1:
+        nodo_nuevo1 = nodos.pop(0)
+        nodo_nuevo2 = nodos.pop(0)
+        nuevo = nodo_nuevo1.valor + nodo_nuevo2.valor
+        nodo_nuevo3 = NodoHuffman(nuevo, None, nodo_nuevo1, nodo_nuevo2)
+        nodos.append(nodo_nuevo3)
+        nodos = sorted(nodos, key=lambda nodo: nodo.valor)
+    
+    return nodos[0]
+
+
+def codificar(mensaje, diccionario):
+    mensaje_comprimido = ''
+    for caracter in mensaje:
+        mensaje_comprimido += diccionario.get(caracter)
+    return mensaje_comprimido
+
+
+def decodificar(arbol, mensaje):
+    mensajeDecodificado = ''
+    raiz = arbol
+    while len(mensaje) >= 1:
+        while not hoja(raiz):
+            if len(mensaje) > 0:
+                caracter = mensaje[0]
+                mensaje = mensaje[1:]
+            if caracter == "0":
                 raiz = raiz.izq
             else:
-                raiz.izq, aux = reemplazar(raiz.izq)
-                raiz.info = aux.info
-        return(raiz, x)
+                raiz = raiz.der
+        mensajeDecodificado += raiz.info
+        raiz = arbol
+    return mensajeDecodificado
+
+
+def generarNiveles(niveles=0):
+    arbol = None
+    arbol = insertar(arbol, random.randint(0, 1000))
+    while arbol.altura <= niveles:
+        arbol = insertar(arbol, random.randint(0, 1000))
+    return arbol
+
+
+def cortarNiveles(raiz, bosque, nivel, actual=0):
+    if raiz is not None:
+        if actual <= nivel:
+            if actual == nivel:
+                bosque.append(raiz)
+            cortarNiveles(raiz.izq, bosque, nivel, actual+1)
+            cortarNiveles(raiz.der, bosque, nivel, actual+1)
+
+
+def arbolcompleto(raiz):
+    nodos = 0
+    for i in range(altura(raiz)):
+        nodos += 2**i
+    if nodos == pesoArbol(raiz):
+        return True
+    else:
+        return False
+
+    
+def morse_decod(arbol, codigo):
+    aux = arbol
+    msj_decod = ''
+    for i in range(len(codigo)):
+        if codigo[i] == ' ':
+            msj_decod = msj_decod + aux.info
+            aux = arbol
+        if codigo[i] == '.':
+            if aux.izq is None:
+                msj_decod = msj_decod + aux.info
+                aux = arbol
+            else:
+                aux = aux.izq
+        if codigo[i] == '-':
+            if aux.der is None:
+                msj_decod = msj_decod + aux.info
+                aux = arbol
+            else:
+                aux = aux.der
+        if codigo[i] == '/':
+            msj_decod = msj_decod + ' '
+            aux = arbol
+    return msj_decod
+
+
+def HeroesyMisiones():
+    r = None
+    lista = ['Itergalactica', ['Guardianas de la galaxia', 'Capitana Marvel'], 'En equipo', 'Guardianas de la galaxia', 'Recuperacion', ['AntMan', 'Capitan America', 'Black Widow', 'SpiderMan'], 'Habilidoso', ['SpiderMan', 'AntMan'], 'Defensa', ['Capitan America', 'SpiderMan', 'IronMan'], 'Estrategia', ['IronMan', 'Doctor Strange'], 'Destruccion', ['Thor, Hulk'], 'Rescate']
+    codigos = [6000, 5000, 4000, 3000, 2000, 1000, 14000, 13000, 10000, 9000, 12000, 11000, 8000, 7000, 16000]
+    for i in range(0, len(lista)):
+        r = insertarDecision(r, lista[i], codigos[i])
+    return r
+
+
+def superheroe_para_mision(mision):
+    r = HeroesyMisiones()
+    decision = False
+    superHeroes = ''
+    while decision is not True and r is not None:
+        if r.info == mision:
+            superHeroes = r.izq.info
+            decision = True
+        else:
+            r = r.der
+    if len(superHeroes) > 0:
+        print('Los superheroes indicados para esta mision son: ')
+        print(superHeroes)
+    else:
+        print('No se encontro o la mision o los superheroes')
+
+
+def mensajeSatelite():
+    estado_del_tiempo = random.choice(['Despejado', 'Nublado', 'Lluvia'])
+    humedad = random.choice(['Baja', 'Alta'])
+    cod1 = random.choice(['1', '2'])
+    cod2 = random.choice(['3', '5'])
+    cod3 = random.choice(['7', '8'])
+    mensaje = estado_del_tiempo + '-' + humedad + '-' + cod1 + '-' + cod2 + '-' + cod3
+    return mensaje
+
+
+def codificar_mensajeSatelite(diccionario, mensaje):
+    mensaje_codificado = ''
+    caracteres = mensaje.split('-')
+    for caracter in caracteres:
+        mensaje_codificado += diccionario.get(caracter)
+    return mensaje_codificado
+
+
+# mensaje 1 codificado y mensaje2 decodificado
+def diferencia_tamanio(mensaje1, mensaje2):
+    tamanio1 = getsizeof(mensaje1)
+    tamanio2 = getsizeof(mensaje2)
+    
+    return tamanio1 - tamanio2
+
+
+def frecuencia(tabla):
+    largo = 0
+    for item in tabla:
+        largo += item[0]
+    for item in tabla:
+        item[2] = item[0] / largo
+
+
+def tiempo_meteorologico(arbol, temperatura, presion, humedad, visibilidad, viento):
+    datos = {
+        'Temperatura': temperatura,
+        'Presion': presion,
+        'Humedad': humedad,
+        'Visibilidad': visibilidad,
+        'Viento': viento
+    }
+    print('Temperatura: ', temperatura)
+    print('Presion: ', presion)
+    print('Humedad: ', humedad)
+    print('Visibilidad: ', visibilidad)
+    print('Viento: ', viento)
+    pronostico = ''
+    while pronostico == '' and arbol is not None:
+        if datos[arbol.info[0]] <= arbol.info[1]:
+            if arbol.izq.izq is not None:
+                arbol = arbol.izq
+            else:
+                pronostico = arbol.izq.info
+                print('Pronostico: ', pronostico)
+        else:
+            if arbol.der.der is not None:
+                arbol = arbol.der
+            else:
+                pronostico = arbol.der.info
+                print('Pronostico: ', pronostico)
